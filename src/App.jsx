b136fa3213fdbdce0603820ccadeefee2257d0be
Vyakhya Goyal <vyakhyatwentytwo@gmail.com>
2025-07-18 22:12:41 +0530

    spline done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
 'use client';
-import React,{useState,useRef,useEffect} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import MousePosition from './MousePosition';
 import './App.css'
-import { motion, useScroll,useTransform } from "motion/react"
+import { motion, useScroll, useTransform } from "motion/react"
 import content from './components/content';
 import Card from './components/card';
+import Spline from '@splinetool/react-spline';
 
 function App() {
-  const ref=useRef();
-  const {scrollYProgress}=useScroll({
-    target: ref,
-    offset: ["start end", "end start"],
-  });
+  const ref = useRef();
+  const { scrollYProgress } = useScroll();
   const clipPath = useTransform(scrollYProgress, [0, 1], ["inset(0 100% 0 0)", "inset(0 0% 0 0)"]);
 
-  const {scrollY}=useScroll();
-  const opacity=useTransform(scrollY, [0, 800], [1, 0]);
+  const { scrollY } = useScroll();
+  const opacity = useTransform(scrollY, [0, 300], [1, 0]);
 
-  const [hovered,setHovered]=useState(false);
+  const [hovered, setHovered] = useState(false);
   const [slow, setSlow] = useState(false);
 
-  const{x,y}=MousePosition();
-  const size=hovered?350:50;
+  const { x, y } = MousePosition();
+  const size = hovered ? 350 : 50;
 
   // State to control scroll lock
-  const [animationDone, setAnimationDone] = useState(false);
-  const [wixTextDone, setWixTextDone] = useState(false);
+  // const [animationDone, setAnimationDone] = useState(false);
+  // const [wixTextDone, setWixTextDone] = useState(false);
 
   // // Lock scroll until animation is done
   // useEffect(() => {
-  //   if (!animationDone || !wixTextDone) {
+  //   if (!wixTextDone) {
   //     document.body.style.overflow = 'hidden';
   //   } else {
   //     document.body.style.overflow = 'auto';
@@ -37,58 +35,66 @@ function App() {
   //   return () => {
   //     document.body.style.overflow = 'auto';
   //   };
-  // }, [animationDone,wixTextDone]);
+  // }, [wixTextDone]);
+useEffect(() => {
+    // Lock scroll
+    document.body.style.overflow = 'hidden';
 
+    // Unlock scroll after 5 seconds
+    const timer = setTimeout(() => {
+      document.body.style.overflow = 'auto';
+    }, 6000);
+
+    // Cleanup
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <>
-      <div className="flex items-center justify-start min-h-screen" style={{backgroundColor: 'black'}}>
-        <div className="text-8xl mb-100 ml-20 mt-25 items-center" style={{ flex: 1 }}>
+      <div className="flex items-center justify-start min-h-screen" style={{ backgroundColor: 'black' }}>
+        <div className="text-8xl mb-100 ml-20 mt-25 items-center">
 
-          <motion.h1
-            className='main mb-150'
-            style={{ clipPath }}
-            // onAnimationComplete={() => setWixTextDone(true)}
+          {/* <motion.h1
+
+            style={{ clipPath, color: 'white' }}
+            onAnimationComplete={() => setWixTextDone(true)}
           >
             Wix studio<br />
 
-          </motion.h1>
+          </motion.h1> */}
 
-          <motion.h1
+          {/* <div style={{ width: "fit-content" }} className='text-8xl'>
+            <h1 className="main">
+              Hello hi
+            </h1>
+            <motion.h1
+              className="text-overlay"
+              style={{ clipPath }}
+            >
+              Hello hi
+            </motion.h1>
+          </div> */}
+
+          {/* <motion.h1
             style={{
               opacity,
               color: "white",
             }} className='mb-100'
-            ref={ref}
+            
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 2 }}
-            // wixTextDone={() => setWixTextDone(true)}
+          // wixTextDone={() => setWixTextDone(true)}
           >
             Wix studio<br />
             Deliver brilliance.<br />
             Smash deadlines.
-          </motion.h1>
-          
+          </motion.h1> */}
+
         </div>
-        <div className="flex-1 flex justify-center items-center">
-          <iframe
-            src="https://clear-beautifully-265808.framer.app/"
-            width="600"
-            height="600"
-            style={{ background: "black" }}
-            title="Framer Project"
-          />
-        </div>
+        
       </div>
 
-    <motion.div className="div">
-      <motion.h1 className='text-overlay'>Wix studio
-      <br />
-        Deliver brilliance.
-        <br />
-        Smash deadlines.</motion.h1>
-    </motion.div>
       {/* <header className='header bg-gray-200 text-black flex-col italic'>welcome</header> */}
       {/* <h1 className='text-blue-500 bg-red-300 font-bold underline flex flex-col'>framer motion</h1>
       <motion.div 
@@ -127,28 +133,28 @@ function App() {
 </motion.div> */}
       <motion.div className='mask-container h-100vh bg-black text-stone-400 m-0 font-serif relative'>
         <motion.div className="mask h-screen flex justify-center items-center text-8xl cursor-default absolute top-0 left-0 w-full h-full z-10"
-        animate={{
-          maskPosition:`${x-size/2}px ${y-size/2}px`,
-          maskSize:`${size}px`
-        }}
-        transition={{
-          type:'tween',
-          ease:'backOut',
-          duration:slow?1.5:0.5,
-        }}>
-          <p onMouseEnter={() => {setHovered(true); setSlow(true);}} onMouseLeave={() => {setHovered(false); setSlow(false);}}>THE EVIL TWIN.</p>
+          animate={{
+            maskPosition: `${x - size / 2}px ${y - size / 2}px`,
+            maskSize: `${size}px`
+          }}
+          transition={{
+            type: 'tween',
+            ease: 'backOut',
+            duration: slow ? 1.5 : 0.5,
+          }}>
+          <p onMouseEnter={() => { setHovered(true); setSlow(true); }} onMouseLeave={() => { setHovered(false); setSlow(false); }}>THE EVIL TWIN.</p>
         </motion.div>
         <motion.div className="body h-screen flex justify-center items-center text-8xl cursor-default z-0">
           <p>THIS IS <span className='text-rose-600'>VYAKHYA</span>.</p>
         </motion.div>
       </motion.div>
-      
+
       {content.map((content, index) => {
         return (
           <Card key={index} {...content} />
         )
       })}
-      
+
       {/* <footer className='footer mx-2 my-4 text-red-800'>contact us</footer> */}
     </>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import content from './content'
 const Card = ({title,description,color}) => {
     return (
         <div className="card-container mb-100" style={{position: 'sticky', top: '0px'}}>
-                <div className='card' style={{height:'100vh', width:'200vh', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', backgroundColor:color, }}>
+                <div className='card' style={{height:'100vh', width:'200vh', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', backgroundColor:color, borderRadius:'25px' }}>
                     {/* <div className='card-color' style={{ backgroundColor: color }}></div> */}
                     <h2 className='card-title'>{title}</h2>
                     <p className='card-content'>{description}</p>
